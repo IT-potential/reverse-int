1bd1490f0affa700490d8a6df0ec7483c86f6ec3
Zlata Romanova <forofjob@gmail.com>
2023-02-19 02:20:56 +0700

    the task finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num = Math.abs(n);
+    let number = num.toString().split("");
+    let arr = [];
+    for (let el of number) {
+        arr.unshift(el);
+    }
+    return +arr.join("");
+};
